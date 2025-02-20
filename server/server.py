e6224a250eb936c37c2004752013fc8599f2d8ab
Excalibur <104138693+DSR3164@users.noreply.github.com>
2025-02-21 01:48:29 +0700

    Update gunicorn timeout for upload videos

--- server/server.py
+++ server/server.py
@@ -16,6 +16,8 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////instance/database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 500MB
+
 
 jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
