994d14c36f0ffcc6225cc626773c09fe7b1fcc2d
Excalibur <104138693+DSR3164@users.noreply.github.com>
2025-02-20 23:54:57 +0700

    Update DB

--- server/server.py
+++ server/server.py
@@ -14,13 +14,14 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("MYSQL_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
 db.init_app(app)
 CORS(app)
+db.create_all()
 
 # Папки для хранения медиа
 VIDEO_FOLDER = 'media/videos'
